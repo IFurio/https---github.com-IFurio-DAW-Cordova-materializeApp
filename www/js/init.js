11a92264642a3c7faa3726ebcaebe5c928caba3c
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-02-19 21:28:18 +0100

    in progress generate random with ajax

--- www/js/init.js
+++ www/js/init.js
@@ -18,3 +18,20 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
 }
+
+$('#generate').click(function() {
+  // fetch 10 dogs pics
+  $.ajax({
+    method: "GET",
+    url: "https://dog.ceo/api/breeds/image/random/10",
+    dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+  }).done(function (msg) {
+    // $("#test-swipe-2") hacer aqui lo de añadir las fotos de los perritos
+    for(let item in msg.message) {
+      console.log(msg.message[item]);
+
+    };
+  }).fail(function () {
+    alert("ERROR");
+  });
+});
